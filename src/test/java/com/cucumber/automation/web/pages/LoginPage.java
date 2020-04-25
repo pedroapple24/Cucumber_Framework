dd9bde7da1c7c07123b61b9057ed48b329b28cce
pedroapple24 <rlpedro92@gmail.com>
2020-04-25 00:16:59 -0500

    commit to make it work

--- src/test/java/com/cucumber/automation/web/pages/LoginPage.java
+++ src/test/java/com/cucumber/automation/web/pages/LoginPage.java
@@ -9,22 +9,12 @@ import com.cucumber.automation.utils.DriverFactory;
 
 public class LoginPage extends DriverFactory {
 
-    /**
-     * All locators will be mentioned here
-     *
-     * For this example i am not using properties file for
-     reading locators
-     */
-
     By SigninLink = By.linkText("Sign in");
     By searchField = By.name("q");
     By loginSection = By.className("auth-form-body");
     By SigninButton = By.name("commit");
     By errorMessage = By.xpath("//div[@id='js-flash-container']/div/div");
-
-    /**
-     * All functions related to behavior will follow now
-     */
+    
     public void ishomepageDisplayed() {
         waitVar.until(ExpectedConditions.presenceOfElementLocated(SigninLink));
         driver.findElement(SigninLink).isDisplayed();
